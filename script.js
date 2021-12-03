e48036fbe56631a1657ba86bae0f0166ffb614e0
Luiz Victor <luizvictor1231@gmail.com>
2021-12-03 16:06:45 -0400

    Convertendo Celsius para Fahrenheit e Fahrenheit para Celsius :)

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function converter() {
-  var celsius_element = document.getElementById('converter-btn')
+  var celsius_element = document.getElementById('celsius')
   var celsius = Number(celsius_element.value)
 
   var valorEmFahrenheit = (celsius * 9) / 5 + 32
-  document.getElementById('fahrenheit').innerHTML = valorEmFahrenheit
+  document.getElementById('fahrenheit').value = valorEmFahrenheit
 }
 
-var input = document.getElementById('converter-btn')
+function converterFarenheit() {
+  var fahrenheit_element = document.getElementById('fahrenheit')
+  var fahrenheit = Number(fahrenheit_element.value)
+
+  var valorEmCelsius = ((fahrenheit - 32) * 5) / 9
+  document.getElementById('celsius').value = valorEmCelsius.toFixed(2)
+}
+
+var input = document.getElementById('celsius')
 input.addEventListener('input', () => {
   converter()
+})
+
+var input_fahrenheit = document.getElementById('fahrenheit')
+input_fahrenheit.addEventListener('input', () => {
+  converterFarenheit()
 })
